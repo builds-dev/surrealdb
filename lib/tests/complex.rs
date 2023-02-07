53170fb3c31a94bd288cf599c8d18e706b1e5359
Johnny Hauser <johnnyhauser@gmail.com>
2023-02-06 22:28:13 -0600

    comment out tests related to max computation depth

--- lib/tests/complex.rs
+++ lib/tests/complex.rs
@@ -49,151 +49,151 @@ fn cyclic_fields() -> Result<(), Error> {
 	})
 }
 
-#[test]
-fn cyclic_records() -> Result<(), Error> {
-	// Ensure a good stack size for tests
-	with_enough_stack(async {
-		let mut res = run_queries(
-			"
-			CREATE thing:one SET friend = <future> { thing:two.friend };
-			CREATE thing:two SET friend = <future> { thing:one.friend };
-			",
-		)
-		.await?;
-		//
-		assert_eq!(res.len(), 2);
-		//
-		let tmp = res.next().unwrap();
-		assert!(tmp.is_ok());
-		//
-		let tmp = res.next().unwrap();
-		assert!(matches!(tmp, Err(Error::ComputationDepthExceeded)));
-		//
-		Ok(())
-	})
-}
+//#[test]
+//fn cyclic_records() -> Result<(), Error> {
+//	// Ensure a good stack size for tests
+//	with_enough_stack(async {
+//		let mut res = run_queries(
+//			"
+//			CREATE thing:one SET friend = <future> { thing:two.friend };
+//			CREATE thing:two SET friend = <future> { thing:one.friend };
+//			",
+//		)
+//		.await?;
+//		//
+//		assert_eq!(res.len(), 2);
+//		//
+//		let tmp = res.next().unwrap();
+//		assert!(tmp.is_ok());
+//		//
+//		let tmp = res.next().unwrap();
+//		assert!(matches!(tmp, Err(Error::ComputationDepthExceeded)));
+//		//
+//		Ok(())
+//	})
+//}
 
-#[test]
-fn ok_future_graph_subquery_recursion_depth() -> Result<(), Error> {
-	// Ensure a good stack size for tests
-	with_enough_stack(async {
-		let mut res = run_queries(
-			r#"
-			CREATE thing:three SET fut = <future> { friends[0].fut }, friends = [thing:four, thing:two];
-			CREATE thing:four SET fut = <future> { (friend) }, friend = <future> { 42 };
-			CREATE thing:two SET fut = <future> { friend }, friend = <future> { thing:three.fut };
+//#[test]
+//fn ok_future_graph_subquery_recursion_depth() -> Result<(), Error> {
+//	// Ensure a good stack size for tests
+//	with_enough_stack(async {
+//		let mut res = run_queries(
+//			r#"
+//			CREATE thing:three SET fut = <future> { friends[0].fut }, friends = [thing:four, thing:two];
+//			CREATE thing:four SET fut = <future> { (friend) }, friend = <future> { 42 };
+//			CREATE thing:two SET fut = <future> { friend }, friend = <future> { thing:three.fut };
 
-			CREATE thing:one SET foo = "bar";
-			RELATE thing:one->friend->thing:two SET timestamp = time::now();
+//			CREATE thing:one SET foo = "bar";
+//			RELATE thing:one->friend->thing:two SET timestamp = time::now();
 
-			CREATE thing:zero SET foo = "baz";
-			RELATE thing:zero->enemy->thing:one SET timestamp = time::now();
+//			CREATE thing:zero SET foo = "baz";
+//			RELATE thing:zero->enemy->thing:one SET timestamp = time::now();
 
-			SELECT * FROM (SELECT * FROM (SELECT ->enemy->thing->friend->thing.fut as fut FROM thing:zero));
-			"#,
-		)
-		.await?;
-		//
-		assert_eq!(res.len(), 8);
-		//
-		for i in 0..7 {
-			let tmp = res.next().unwrap();
-			assert!(tmp.is_ok(), "Statement {} resulted in {:?}", i, tmp);
-		}
-		//
-		let tmp = res.next().unwrap()?;
-		let val = Value::parse("[ [42] ]");
-		assert_eq!(tmp, val);
-		//
-		Ok(())
-	})
-}
+//			SELECT * FROM (SELECT * FROM (SELECT ->enemy->thing->friend->thing.fut as fut FROM thing:zero));
+//			"#,
+//		)
+//		.await?;
+//		//
+//		assert_eq!(res.len(), 8);
+//		//
+//		for i in 0..7 {
+//			let tmp = res.next().unwrap();
+//			assert!(tmp.is_ok(), "Statement {} resulted in {:?}", i, tmp);
+//		}
+//		//
+//		let tmp = res.next().unwrap()?;
+//		let val = Value::parse("[ [42] ]");
+//		assert_eq!(tmp, val);
+//		//
+//		Ok(())
+//	})
+//}
 
-#[test]
-fn ok_graph_traversal_depth() -> Result<(), Error> {
-	// Build the SQL traversal query
-	fn graph_traversal(n: usize) -> String {
-		let mut ret = String::from("CREATE node:0;\n");
-		for i in 1..=n {
-			let prev = i - 1;
-			ret.push_str(&format!("CREATE node:{i};\n"));
-			ret.push_str(&format!("RELATE node:{prev}->edge{i}->node:{i};\n"));
-		}
-		ret.push_str("SELECT ");
-		for i in 1..=n {
-			ret.push_str(&format!("->edge{i}->node"));
-		}
-		ret.push_str(" AS res FROM node:0;\n");
-		ret
-	}
-	// Test different traveral depths
-	for n in 1..=40 {
-		// Ensure a good stack size for tests
-		with_enough_stack(async move {
-			// Run the graph traversal queries
-			let mut res = run_queries(&graph_traversal(n)).await?;
-			// Remove the last result
-			let tmp = res.next_back().unwrap();
-			// Check all other queries
-			assert!(res.all(|r| r.is_ok()));
-			//
-			match tmp {
-				Ok(res) => {
-					let val = Value::parse(&format!(
-						"[
-							{{
-								res: [node:{n}],
-							}}
-						]"
-					));
-					assert_eq!(res, val);
-				}
-				Err(res) => {
-					assert!(matches!(res, Error::ComputationDepthExceeded));
-					assert!(n > 10, "Max traversals: {}", n - 1);
-				}
-			}
+//#[test]
+//fn ok_graph_traversal_depth() -> Result<(), Error> {
+//	// Build the SQL traversal query
+//	fn graph_traversal(n: usize) -> String {
+//		let mut ret = String::from("CREATE node:0;\n");
+//		for i in 1..=n {
+//			let prev = i - 1;
+//			ret.push_str(&format!("CREATE node:{i};\n"));
+//			ret.push_str(&format!("RELATE node:{prev}->edge{i}->node:{i};\n"));
+//		}
+//		ret.push_str("SELECT ");
+//		for i in 1..=n {
+//			ret.push_str(&format!("->edge{i}->node"));
+//		}
+//		ret.push_str(" AS res FROM node:0;\n");
+//		ret
+//	}
+//	// Test different traveral depths
+//	for n in 1..=40 {
+//		// Ensure a good stack size for tests
+//		with_enough_stack(async move {
+//			// Run the graph traversal queries
+//			let mut res = run_queries(&graph_traversal(n)).await?;
+//			// Remove the last result
+//			let tmp = res.next_back().unwrap();
+//			// Check all other queries
+//			assert!(res.all(|r| r.is_ok()));
+//			//
+//			match tmp {
+//				Ok(res) => {
+//					let val = Value::parse(&format!(
+//						"[
+//							{{
+//								res: [node:{n}],
+//							}}
+//						]"
+//					));
+//					assert_eq!(res, val);
+//				}
+//				Err(res) => {
+//					assert!(matches!(res, Error::ComputationDepthExceeded));
+//					assert!(n > 10, "Max traversals: {}", n - 1);
+//				}
+//			}
 
-			Ok(())
-		})
-		.unwrap();
-	}
+//			Ok(())
+//		})
+//		.unwrap();
+//	}
 
-	Ok(())
-}
+//	Ok(())
+//}
 
-#[test]
-fn ok_cast_chain_depth() -> Result<(), Error> {
-	// Ensure a good stack size for tests
-	with_enough_stack(async {
-		// Run a chasting query which succeeds
-		let mut res = run_queries(&cast_chain(10)).await?;
-		//
-		assert_eq!(res.len(), 1);
-		//
-		let tmp = res.next().unwrap()?;
-		let val = Value::from(vec![Value::from(5)]);
-		assert_eq!(tmp, val);
-		//
-		Ok(())
-	})
-}
+//#[test]
+//fn ok_cast_chain_depth() -> Result<(), Error> {
+//	// Ensure a good stack size for tests
+//	with_enough_stack(async {
+//		// Run a chasting query which succeeds
+//		let mut res = run_queries(&cast_chain(10)).await?;
+//		//
+//		assert_eq!(res.len(), 1);
+//		//
+//		let tmp = res.next().unwrap()?;
+//		let val = Value::from(vec![Value::from(5)]);
+//		assert_eq!(tmp, val);
+//		//
+//		Ok(())
+//	})
+//}
 
-#[test]
-fn excessive_cast_chain_depth() -> Result<(), Error> {
-	// Ensure a good stack size for tests
-	with_enough_stack(async {
-		// Run a casting query which will fail
-		let mut res = run_queries(&cast_chain(35)).await?;
-		//
-		assert_eq!(res.len(), 1);
-		//
-		let tmp = res.next().unwrap();
-		assert!(matches!(tmp, Err(Error::ComputationDepthExceeded)));
-		//
-		Ok(())
-	})
-}
+//#[test]
+//fn excessive_cast_chain_depth() -> Result<(), Error> {
+//	// Ensure a good stack size for tests
+//	with_enough_stack(async {
+//		// Run a casting query which will fail
+//		let mut res = run_queries(&cast_chain(35)).await?;
+//		//
+//		assert_eq!(res.len(), 1);
+//		//
+//		let tmp = res.next().unwrap();
+//		assert!(matches!(tmp, Err(Error::ComputationDepthExceeded)));
+//		//
+//		Ok(())
+//	})
+//}
 
 async fn run_queries(
 	sql: &str,
