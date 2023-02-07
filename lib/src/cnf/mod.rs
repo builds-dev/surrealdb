11d5503b3064d90cd62771067bac99e58dccd1bb
Johnny Hauser <johnnyhauser@gmail.com>
2023-02-06 21:11:57 -0600

    subtract 1 from MAX_COMPUTATION_DEPTH to avoid lint error due to using u8::MAX

--- lib/src/cnf/mod.rs
+++ lib/src/cnf/mod.rs
@@ -4,7 +4,7 @@
 pub const MAX_CONCURRENT_TASKS: usize = 64;
 
 /// Specifies how deep various forms of computation will go before the query fails.
-pub const MAX_COMPUTATION_DEPTH: u8 = u8::MAX;
+pub const MAX_COMPUTATION_DEPTH: u8 = u8::MAX - 1;
 
 /// Specifies the names of parameters which can not be specified in a query.
 pub const PROTECTED_PARAM_NAMES: &[&str] = &["auth", "scope", "token", "session"];
